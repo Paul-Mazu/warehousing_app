83f3842ff02d446379e1b0e1fdba347b269e8a38
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2022-10-30 12:31:41 +0100

    separate ordering logic to function, repair whole logic

--- query.py
+++ query.py
@@ -15,7 +15,7 @@ def menu():
             '\nWhat would you like to do?\n'
             '1. List items by warehouse\n'
             '2. Search an item and place an order\n'
-            '3. Quit\n' 
+            '3. Quit\n'
             'Type the number of the operation: '))
 
         if user_choice == 1:
@@ -47,8 +47,11 @@ def searching_engine(item):
 
 def order_max_or_not():
     while True:
-        choice = int(input('Would you like to order:\n1. maximum items\n2. different number of items\n3. check '
-                           'another item\n4. return to menu:\n'))
+        choice = int(input('Would you like to order:\n'
+                           '1. maximum items\n'
+                           '2. different number of items\n'
+                           '3. check another item\n'
+                           '4. return to menu:\n'))
         if choice in range(1, 5):
             return choice
         else:
@@ -56,18 +59,16 @@ def order_max_or_not():
 
 
 def want_check_another_item():
-    check_other_item = input(f"Would you like to check another item {user_name}? (y/n): ")
+    check_other_item = input(f'Would you like to check another item {user_name}? (y/n): ')
     if check_other_item == "n":
         return "n"
 
 
 def ordering_logic():
-
     back_to_menu = False
     while not back_to_menu:
 
         item_to_search = input("What is the name of the item? ")
-
         available_items_dict = searching_engine(item_to_search)
         sum_of_available_items = sum(available_items_dict.values())
 
@@ -76,76 +77,62 @@ def ordering_logic():
             print(f"Amount available: {sum_of_available_items}")
 
             for k, v in available_items_dict.items():
-                print(f"In {k} available is: {v} items")
+                print(f'In {k} available is: {v} items')
 
             ordering_decision = input(
                 "Would you like to order this item? (y/n): ")
 
-            if ordering_decision == "y":
-
-                correct_order = False
-
-                while not correct_order:
-                    number_of_items_to_order = int(
-                        input("How many items would you like to order?: "))
-
-                    if sum_of_available_items >= number_of_items_to_order > 0:
-                        print(f"{number_of_items_to_order} of {item_to_search} have been ordered")
-                        correct_order = True
-
-                    elif number_of_items_to_order > sum_of_available_items:
-                        print(f"There is only {sum_of_available_items} items available")
-                        answer = order_max_or_not()
-                        quick_back_to_menu = False  # quick go to menu
-                        quick_check_another_item = False
-
-                        if answer == 1:
-                            print(f"{sum_of_available_items} of {item_to_search} have been ordered")
-                            correct_order = True
-
-                        elif answer == 2:
-                            while True:
-                                how_much_to_order = int(input("How much to order?: "))
-
-                                if 0 < how_much_to_order <= sum_of_available_items:
-                                    print(f"{how_much_to_order} {item_to_search} have been ordered")
-                                    correct_order = True
-                                    break
-
-                                elif how_much_to_order <= 0:
-                                    print("The number of items is incorect")
-                                    if input("Do you still want to order the item? (y/n)") == "n":
-                                        break
-
-                                else:
-                                    print("You have to choose available amount")
-                        elif answer == 3:
-                            correct_order = True
-                            quick_check_another_item = True
-
-                        elif answer == 4:
-                            correct_order = True
-                            quick_back_to_menu = True
-
-                    else:
-                        print("The number of items is incorect")
-                        if input("Do you still want to order the item? (y/n)") == "n":
-                            correct_order = True
-
-                if not quick_back_to_menu:
-                    if not quick_check_another_item:
-                        if want_check_another_item() == "n":
-                            back_to_menu = True
-                else:
+            if ordering_decision == 'y':
+                check_another = ordering_items(item_to_search, sum_of_available_items)
+                if check_another == 'check another':
+                    continue
+                elif check_another:
+                    back_to_menu = True
+                elif want_check_another_item() == 'n':
                     back_to_menu = True
 
             else:
-                if want_check_another_item() == "n":
+                if want_check_another_item() == 'n':
                     back_to_menu = True
         else:
             print("This item is unavailable")
-            if want_check_another_item() == "n":
+            if want_check_another_item() == 'n':
                 back_to_menu = True
+
+
+def ordering_items(item, sum_of_available_items):
+    correct_order = False
+    while not correct_order:
+
+        number_of_items_to_order = int(input("How many items would you like to order?: "))
+
+        if sum_of_available_items >= number_of_items_to_order > 0:
+            print(f"{number_of_items_to_order} of {item} have been ordered")
+            correct_order = True
+
+        elif number_of_items_to_order < 1:
+            print("The number of items is incorrect")
+            if input("Do you still want to order the item? (y/n)") == "n":
+                correct_order = True
+
+        else:
+            print(f"There is only {sum_of_available_items} items available")
+            answer = order_max_or_not()
+
+            if answer == 1:
+                print(f"{sum_of_available_items} of {item} have been ordered")
+                correct_order = True
+
+            elif answer == 2:
+                print(f'You can order maximally {sum_of_available_items} items')
+
+            elif answer == 3:
+                return 'check another'
+
+            elif answer == 4:
+                return True
+
+    return
 
 
 start()
